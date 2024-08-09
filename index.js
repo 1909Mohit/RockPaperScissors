2d13f06402bacc8b7301bff512c212bbce6f83ec
1909Mohit <mohit1909.mk@gmail.com>
2024-08-09 11:58:00 +0530

    added ui to the game merge rps-ui branch to main

--- index.js
+++ index.js
@@ -1,56 +1,121 @@
-function playGame() {
-  // get a random choice from computer
-  const choices = ["Rock", "Paper", "Scissors"];
+// Global Declarations
 
-  function getComputerChoice() {
-    const choicesLen = choices.length;
+const choices = ["Rock", "Paper", "Scissors"];
 
-    const computerChoice = Math.floor(Math.random() * choicesLen);
-    console.log("computer choice", choices[computerChoice]);
-    return computerChoice;
-  }
+// declare variables to keep track of scores
+let userScore = 0;
+let computerScore = 0;
 
-  //  ask user for its choice
-  function getUserChoice() {
-    const userChoice = prompt("Choose 1 for Rock, 2 for Paper, or 3 for Scissors");
-    console.log("user choice", choices[userChoice - 1]);
-    return userChoice - 1;
-  }
+const scorediv = document.querySelector(".score");
+const score = document.createElement('div');
+scorediv.appendChild(score);
 
-  // declare variables to keep track of scores
-  let userScore = 0;
-  let computerScore = 0;
+const menu = document.querySelector(".choices");
 
-  // Function to play in rounds
-  let round = 1;
+let userChoice;
 
-  function playRound(userChoice, computerChoice) {
-    console.log(`-------- Round: ${round} --------`);
-    // winning logic
-    if(userChoice === computerChoice){
-        console.log(`Its a tie. Both player chose ${choices[userChoice]}`);
-    }
-    else if( (choices[userChoice] === "Rock" && choices[computerChoice] === "Scissors") 
-        || (choices[userChoice] === "Paper" && choices[computerChoice] === "Rock") 
-        || (choices[userChoice] === "Scissors" && choices[computerChoice] === "Paper")
-    ){
-        console.log(`User won! ${choices[userChoice]} beats ${choices[computerChoice]}, Round: ${round}`);
-        userScore++;
-    }
-    else{ 
-        console.log(`User lost! ${choices[computerChoice]} beats ${choices[userChoice]}, Round: ${round}`);
-        computerScore++;
-    }
-    round = round + 1;
-  }
+let round = 1;
 
-  // continue or not
-  let nextRound = "yes";
-  while (nextRound.toLowerCase() === "yes") {
-      playRound(getUserChoice(), getComputerChoice());
-      
-      console.log(`Your Score: ${userScore}, Computer Score: ${computerScore}`);
+function getComputerChoice() {
+  const choicesLen = choices.length;
 
-    nextRound = prompt("Wanna play one more round? Type Yes or No\nNOTE!!!! Anything else other than yes will be considered as a - 'no'");
-  }
+  const computerChoice = Math.floor(Math.random() * choicesLen);
+  console.log("computer choice", choices[computerChoice]);
+  return computerChoice;
 }
+
+menu.addEventListener("click", (event) => {
+  const target = event.target;
+  switch (target.id) {
+    case "rock": {
+      userChoice = 0;
+      playRound(userChoice, getComputerChoice());
+      break;
+    }
+    case "paper": {
+      userChoice = 1;
+      playRound(userChoice, getComputerChoice());
+      break;
+    }
+    case "scissors": {
+      userChoice = 2;
+      playRound(userChoice, getComputerChoice());
+      break;
+    }
+  }
+});
+
+function playRound(userChoice, computerChoice) {
+  const roundText = document.createElement("h3");
+  roundText.innerHTML = `Round: ${round}`;
+  console.log(`-------- Round: ${round} --------`);
+
+  score.appendChild(roundText);
+
+  // winning logic
+  if (userChoice === computerChoice) {
+
+    const displayScore = document.createElement("h3");
+    displayScore.textContent = `Its a tie. Both player chose ${choices[userChoice]}`;
+    console.log(`Its a tie. Both player chose ${choices[userChoice]}`);
+    score.appendChild(displayScore);
+
+    const Scores = document.createElement("h2");
+    Scores.textContent = `Your Score: ${userScore}, Computer Score: ${computerScore}`;
+    score.appendChild(Scores);
+    console.log(`Your Score: ${userScore}, Computer Score: ${computerScore}`);
+  } 
+  else if ( (choices[userChoice] === "Rock" && choices[computerChoice] === "Scissors") ||
+    (choices[userChoice] === "Paper" && choices[computerChoice] === "Rock") ||
+    (choices[userChoice] === "Scissors" && choices[computerChoice] === "Paper") ) 
+  {
+    const displayScore = document.createElement("h3");
+    displayScore.textContent = `User won! ${choices[userChoice]} beats ${choices[computerChoice]}`;
+    score.appendChild(displayScore);
+    console.log(
+      `User won! ${choices[userChoice]} beats ${choices[computerChoice]}`
+    );
+    userScore++;
+
+    const Scores = document.createElement("h2");
+    Scores.textContent = `Your Score: ${userScore}, Computer Score: ${computerScore}`;
+    score.appendChild(Scores);
+    console.log(`Your Score: ${userScore}, Computer Score: ${computerScore}`);
+  }
+  else {
+    const displayScore = document.createElement("h3");
+    displayScore.textContent = `User lost! ${choices[computerChoice]} beats ${choices[userChoice]}`;
+    score.appendChild(displayScore);
+    console.log(
+      `User lost! ${choices[computerChoice]} beats ${choices[userChoice]}`
+    );
+    computerScore++;
+    const Scores = document.createElement("h2");
+    Scores.textContent = `Your Score: ${userScore}, Computer Score: ${computerScore}`;
+    score.appendChild(Scores);
+    console.log(`Your Score: ${userScore}, Computer Score: ${computerScore}`);
+  }
+
+  if (round === 5) {
+    if (userScore > computerScore) {
+      const displayScore = document.createElement("h2");
+      displayScore.textContent = `Hurray!!!! You have won!!`;
+      score.appendChild(displayScore);
+    } else if (userScore < computerScore) {
+      const displayScore = document.createElement("h2");
+      displayScore.textContent = `Oops! You lost!!`;
+      score.appendChild(displayScore);
+    } else {
+      const displayScore = document.createElement("h2");
+      displayScore.textContent = `It's a tie!!`;
+      score.appendChild(displayScore);
+    }
+    
+  }
+  round = round + 1;
+}
+
+const displayScore = document.createElement("h2");
+displayScore.textContent = `Your Score: ${userScore}, Computer Score: ${computerScore}`;
+score.appendChild(displayScore);
+console.log(`Your Score: ${userScore}, Computer Score: ${computerScore}`);
